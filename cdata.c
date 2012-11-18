cfdecf7d46bf0433dce2901f912ec619488a4cc8
simon.wang <simonnn.wang@gmail.com>
2012-11-18 14:47:43 +0800

    Add write system call
    1. cdata.c
    2. test.c

--- cdata.c
+++ cdata.c
@@ -6,6 +6,9 @@
 #include <linux/miscdevice.h>
 #include <linux/wait.h>
 #include <asm/io.h>
+/* for copy_to_user()/copy_from_user() */
+#include <asm/uaccess.h>
+/* for ioctl */
 #include "cdata_ioctl.h"
 
 #ifdef CONFIG_SMP
@@ -13,28 +16,48 @@
 #endif
 
 #define	CDATA_MAJOR 121 
+#define BUFFER_SIZE 1024
+
+
+struct cdata_t {
+	char data[BUFFER_SIZE];
+	int index;
+};
 
 
 static int cdata_open(struct inode *inode, struct file *filp)
 {
 	int minor;
+	struct cdata_t *cdata;
 
 	minor = MINOR(inode->i_rdev);
-	printk(KERN_ALERT "cdata: open(minor=%d)\n", minor);
-	printk(KERN_ALERT "cdata: open(file struct address=%p)\n", filp);
+	//printk(KERN_ALERT "cdata: open(minor=%d)\n", minor);
+	//printk(KERN_ALERT "cdata: open(file struct address=%p)\n", filp);
+
+	/* initial cdata structure */
+	cdata = (struct cdata_t *)kmalloc(sizeof(struct cdata_t), GFP_KERNEL);
+	memset(cdata->data, '\0', BUFFER_SIZE);
+	cdata->index = 0;
+	
+	filp->private_data = (void *)cdata;
 	return 0;
 }
 
 static int cdata_ioctl(struct inode *inode, struct file *filp, 
 			unsigned int cmd, unsigned long arg)
 {
-	printk(KERN_ALERT "cdata: ioctl(cmd=%d)\n", cmd);
+	struct cdata_t *cdata = (struct cdata_t *)filp->private_data;
+
+	//printk(KERN_ALERT "cdata: ioctl(cmd=%d)\n", cmd);
 	switch (cmd) {
 	case IOCTL_EMPTY:
-		printk(KERN_ALERT "cdata: ioctl(IOCTL_EMPTY)\n");
+		//printk(KERN_ALERT "cdata: ioctl(IOCTL_EMPTY)\n");
+		memset(cdata->data, '\0', BUFFER_SIZE);
+		cdata->index = 0;
 		break;
 	case IOCTL_SYNC:
-		printk(KERN_ALERT "cdata: ioctl(IOCTL_SYNC)\n");
+		//printk(KERN_ALERT "cdata: ioctl(IOCTL_SYNC)\n");
+		printk(KERN_ALERT "cdata: ioctl(data:%s, index:%d)\n", cdata->data, cdata->index);
 		break;
 	default:
 		return -ENOTTY;
@@ -53,13 +76,28 @@ static ssize_t cdata_read(struct file *filp, char *buf,
 static ssize_t cdata_write(struct file *filp, const char *buf, 
 				size_t size, loff_t *off)
 {
-	printk(KERN_ALERT "cdata: write(%s)\n", buf);
+	struct cdata_t *cdata = (struct cdata_t *)filp->private_data;
+	int i;
+
+	if (size == 0) return 0;
+
+	for (i = 0; i < size; i++) {
+		if (cdata->index >= BUFFER_SIZE)
+			return -EFAULT;
+
+		if (copy_from_user(&cdata->data[cdata->index++], &buf[i], 1))
+			return -EFAULT;
+	}
+	//printk(KERN_ALERT "cdata: write(%s)\n", buf);
 	return 0;
 }
 
 static int cdata_release(struct inode *inode, struct file *filp)
 {
 	printk(KERN_ALERT "cdata: release\n");
+
+	if (!filp->private_data)
+		kfree(filp->private_data);
 	return 0;
 }
 
--- test.c
+++ test.c
@@ -7,25 +7,30 @@ int main(int argc, char *argv[])
 {
 	int fd;
 	pid_t pid;
+	char buf[64];
 
 	pid = fork();
 	if (pid == 0) {
 		/* parent */
+		sprintf(buf, "[%d]I'm parent", getpid());
 	} else if (pid > 0) {
 		/* child */
+		sprintf(buf, "[%d]I'm child", getpid());
 	} else {
 		printf("fork error");
 		return -1;
 	}
 	
 
-	fd = open("/dev/cdata", O_RDONLY);
+	fd = open("/dev/cdata", O_RDWR);
 	if (fd == -1) {
 		printf("open /dev/cdata error.");
 		return -1;
 	}
 
-	printf("---> ioctl num(%d)\n", IOCTL_EMPTY);
+	write(fd, buf, sizeof(buf));
+	ioctl(fd, IOCTL_SYNC, 0);
+	ioctl(fd, IOCTL_EMPTY, 0);
 	ioctl(fd, IOCTL_SYNC, 0);
 
 	close(fd);
