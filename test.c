4dab46e6004092e511e3745414c636b5ee9473cb
simon.wang <simonnn.wang@gmail.com>
2012-11-25 14:55:10 +0800

    Modify test.c for mmap().

--- test.c
+++ test.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+/* for mmap() */
+#include <sys/mman.h>
 #include "cdata_ioctl.h"
 
 int main(int argc, char *argv[])
@@ -8,7 +10,11 @@ int main(int argc, char *argv[])
 	int fd;
 	pid_t pid;
 	char buf[64];
+	char p[4] = {0x00, 0x00, 0xff, 0x00};	
+	unsigned char *fb;
+	int i;
 
+#if 0
 	pid = fork();
 	if (pid > 0) {
 		/* parent */
@@ -20,7 +26,7 @@ int main(int argc, char *argv[])
 		printf("fork error");
 		return -1;
 	}
-	
+#endif
 
 	fd = open("/dev/cdata", O_RDWR);
 	if (fd == -1) {
@@ -28,10 +34,34 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
+#if 0
+	/* basic test */
 	write(fd, buf, sizeof(buf));
+#endif
+
+	/* mmap() */
+	fb = (unsigned char *)mmap(0, 240*320*4, PROT_READ | PROT_WRITE,
+			MAP_SHARED, fd, 0);
+	for (i = 0; i < 500; i++) {
+		*fb = 0xff; fb++;
+		*fb = 0x00; fb++;
+		*fb = 0x00; fb++;
+		*fb = 0x00; fb++;
+	}
+
+#if 0
+	/* ioremap, kernel timer */
+	while (1) {
+		write(fd, p, 4);
+	}
+#endif
+
+#if 0
+	/* ioctl */
 	ioctl(fd, IOCTL_SYNC, 0);
 	ioctl(fd, IOCTL_EMPTY, 0);
 	ioctl(fd, IOCTL_SYNC, 0);
+#endif
 
 	close(fd);
 	return 0;
